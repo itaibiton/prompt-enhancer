538339e0b97a36e4815706cc88a842064b3272e5
Itai Biton <Itai.Biton2@BankLeumi.co.il>
2025-05-29 17:03:47 +0300

    progress

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,37 +1,87 @@
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger
-} from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
+import {
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectWithLabel
+} from '@/components/ui/select'
+import { CommandIcon, WandSparklesIcon } from 'lucide-react'
+import { Textarea } from './components/ui/textarea'
+import { useState } from 'react'
 
 function App(): JSX.Element {
+  const [prompt, setPrompt] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPrompt(e.target.value)
+  }
+
+  const handleEnhance = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }
+
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your account and remove
-              your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+    <div className="p-4 rounded flex gap-2 flex-col w-[500px] bg-background">
+      <div className="h-8 w-full app-region-drag flex items-center select-none">
+        <span className="font-bold text-primary">Prompt Enhancer</span>
+      </div>
+      <div className="app-region-no-drag flex flex-col gap-2 ">
+        <Textarea
+          className="border border-gray-300 rounded p-2 app-region-no-drag max-h-[200px]"
+          placeholder="Type here..."
+          value={prompt}
+          onChange={handleChange}
+        />
+        <div className="flex w-full gap-4 app-region-no-drag items-end justify-between mt-4">
+          <SelectWithLabel
+            label={
+              <div className="text-xs font-medium text-muted-foreground flex items-center gap-1">
+                Enhance Model
+                <CommandIcon className="w-3 h-3 ml-2" />M
+              </div>
+            }
+            className="border-none"
+          >
+            <SelectTrigger className="text-xs p-0.5 px-2 border-primary/10">
+              <SelectValue placeholder="Select a model" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem className="text-xs p-0.5 px-2 " value="gpt-4o">
+                GPT-4o
+              </SelectItem>
+              <SelectItem className="text-xs p-0.5 px-2 " value="gpt-4o-mini">
+                GPT-4o-mini
+              </SelectItem>
+              <SelectItem className="text-xs p-0.5 px-2 " value="gpt-4">
+                GPT-4
+              </SelectItem>
+              <SelectItem className="text-xs p-0.5 px-2 " value="gpt-3.5-turbo">
+                GPT-3.5-turbo
+              </SelectItem>
+              <SelectItem className="text-xs p-0.5 px-2 " value="gpt-3.5-turbo-1106">
+                GPT-3.5-turbo-1106
+              </SelectItem>
+              <SelectItem className="text-xs p-0.5 px-2 " value="gpt-3.5-turbo-1106-preview">
+                GPT-3.5-turbo-1106-preview
+              </SelectItem>
+            </SelectContent>
+          </SelectWithLabel>
+          <Button
+            size="sm"
+            variant="default"
+            className="p-4 flex items-center gap-2 cursor-pointer"
+            onClick={handleEnhance}
+            disabled={loading}
+          >
+            Enhance <WandSparklesIcon className={`w-4 h-4 ${loading ? 'animate-spin' : ''}`} />
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,6 +4,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// Set dark mode by default
+if (!document.documentElement.classList.contains('dark')) {
+  document.documentElement.classList.add('dark')
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
